a385df5730c9085ea94a6957028d0f1f549f1f26
Raging Beardo <beardos.services@gmail.com>
2023-11-28 23:53:00 -0500

    fix and cleanup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,39 +5,41 @@ mod data;
 
 use data::Config;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn testing_paths() -> String {
-    let exe_path = std::env::current_exe().unwrap();
-    let parent_path = exe_path.parent().unwrap().to_str().unwrap();
-    return String::from(parent_path);
-}
-
-#[tauri::command]
-fn test_write() -> () {
+fn read_current_config() -> () {
     let exe_path = std::env::current_exe().unwrap();
     let parent_path = exe_path.parent().unwrap().to_str().unwrap();
     let dest_path = String::from(parent_path) + "\\config\\config.json";
-    let dest_path_two = String::from(parent_path) + "\\config\\config2.json";
 
     let pulled_config_string = std::fs::read_to_string(&dest_path);
     let read_result = serde_json::from_str::<Config>(&pulled_config_string.unwrap()).unwrap();
 
 
     let _result = std::fs::write(
-        dest_path_two,
+        dest_path,
+        serde_json::to_string_pretty(&read_result).unwrap(),
+    );
+}
+
+#[tauri::command]
+fn write_new_config() -> () {
+    let exe_path = std::env::current_exe().unwrap();
+    let parent_path = exe_path.parent().unwrap().to_str().unwrap();
+    let dest_path = String::from(parent_path) + "\\config\\config.json";
+
+    let pulled_config_string = std::fs::read_to_string(&dest_path);
+    let read_result = serde_json::from_str::<Config>(&pulled_config_string.unwrap()).unwrap();
+
+
+    let _result = std::fs::write(
+        dest_path,
         serde_json::to_string_pretty(&read_result).unwrap(),
     );
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, testing_paths, test_write])
+        .invoke_handler(tauri::generate_handler![read_current_config, write_new_config])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
