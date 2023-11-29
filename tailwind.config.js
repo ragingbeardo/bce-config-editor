a00b4926f61efb5b81aa8f5780414122e76f29e9
Raging Beardo <beardos.services@gmail.com>
2023-11-29 02:37:37 -0500

    i changed my mind i like tailwindcss again

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        rose: colors.rose,
+      }
+    }
+  },
+  plugins: [],
+}
+
