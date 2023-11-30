ac834a50957b5f8d3ee57b4f0b304502019398e0
Raging Beardo <beardos.services@gmail.com>
2023-11-29 22:15:42 -0500

    clean assets and custom scrollwheel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { NgScrollbarModule } from "ngx-scrollbar";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgScrollbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
